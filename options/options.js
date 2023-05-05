db49efd5e6d22c61f052386404b9ae0887b17edb
Crowdbotics <engineering@crowdbotics.com>
2023-05-05 12:18:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-smoke-dev-81436",
+  url: "https://broken-smoke-dev-81436.botics.co",
+  api: "https://broken-smoke-dev-81436.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
